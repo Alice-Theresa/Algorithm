5e5d43d5fc3852edd11fc3a6aa38a1bc89e96e81
Theresa <sc@TheresadeMac-mini.local>
2018-06-30 13:27:12 +0800

    fix bug

--- SelectSort.py
+++ SelectSort.py
@@ -1,16 +1,17 @@
 
-
 def selectSort(array):
     for i in range(0, len(array)):
         minIndex = i
+        temp = array[i]
         for j in range(i + 1, len(array)):
-            if (array[i] > array[j]):
+            if (temp > array[j]):
                 minIndex = j
+                temp = array[j]
         if i != minIndex:
             array[i], array[minIndex] = array[minIndex], array[i]
 
 if __name__ == '__main__':
-    array = [5, 4, 3, 2, 1]
+    array = [5, 4, 6, 3, 2, 1]
     selectSort(array)
     print(array)
 
